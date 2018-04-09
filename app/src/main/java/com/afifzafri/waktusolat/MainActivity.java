3b11e06e11ecadec0473384de53aeeae30d4c15d
Afif Zafri <zafri_21596@yahoo.com>
2018-04-09 19:19:58 +0800

    test set event listener to the select box, and fetch selected item

--- app/src/main/java/com/afifzafri/waktusolat/MainActivity.java
+++ app/src/main/java/com/afifzafri/waktusolat/MainActivity.java
@@ -2,8 +2,12 @@ package com.afifzafri.waktusolat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,19 +16,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize UI elements
+        // Progress Bar
+        final ProgressBar loading = (ProgressBar)findViewById(R.id.loading);
+        // State select box
+        Spinner selectState = (Spinner)findViewById(R.id.selectState);
+        // Zone select box
+        Spinner selectZone = (Spinner)findViewById(R.id.selectZone);
+
         // onload, populate States spinner
         // test array
         String statelist[] = {"Select state...", "Perlis","Kedah","Pulau Pinang","Perak"};
-        Spinner selectState = (Spinner)findViewById(R.id.selectState);
+
         // Populate the spinner with Array values
         ArrayAdapter<String> stateAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, statelist);
         stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
         selectState.setAdapter(stateAdapter);
 
+        selectState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // Check if selected value is index 0, which is no value, do nothing
+                int selectedIndex = adapterView.getSelectedItemPosition();
+                if(selectedIndex != 0) {
+                    loading.setVisibility(View.VISIBLE);// show loading progress bar
+
+                    String selectedVal = adapterView.getSelectedItem().toString();
+                    Toast.makeText(getApplicationContext(), "Selected: "+selectedVal+ " & Index: "+selectedIndex, Toast.LENGTH_SHORT).show();
+
+                    loading.setVisibility(View.GONE); // hide
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         // onload, populate Zone spinner
         // test array
         String zonelist[] = {"Select zone..."};
-        Spinner selectZone = (Spinner)findViewById(R.id.selectZone);
 
         ArrayAdapter<String> zoneAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, zonelist);
         zoneAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
