6ccd3e4c305f5fa1446e44d2f832e12d6ab2530c
Afif Zafri <zafri_21596@yahoo.com>
2018-04-09 14:21:34 +0800

    test populate spinner dropdown with array values

--- app/src/main/java/com/afifzafri/waktusolat/MainActivity.java
+++ app/src/main/java/com/afifzafri/waktusolat/MainActivity.java
@@ -2,6 +2,8 @@ package com.afifzafri.waktusolat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // onload, populate States spinner
+        // test array
+        String statelist[] = {"Select state...", "Perlis","Kedah","Pulau Pinang","Perak"};
+        Spinner selectState = (Spinner)findViewById(R.id.selectState);
+        // Populate the spinner with Array values
+        ArrayAdapter<String> stateAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, statelist);
+        stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        selectState.setAdapter(stateAdapter);
+
+        // onload, populate Zone spinner
+        // test array
+        String zonelist[] = {"Select zone..."};
+        Spinner selectZone = (Spinner)findViewById(R.id.selectZone);
+
+        ArrayAdapter<String> zoneAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, zonelist);
+        zoneAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        selectZone.setAdapter(zoneAdapter);
     }
 }
